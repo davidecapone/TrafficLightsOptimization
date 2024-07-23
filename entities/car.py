14d314e9d1d008e3bee0cac8bdec1e43c76f11db
Davide Capone <davidecapone2304@gmail.com>
2024-07-23 11:33:36 +0200

    Define the car actions, refactor code

--- entities/car.py
+++ entities/car.py
@@ -1,64 +1,66 @@
 import random
 import pygame
+from entities.car_actions import CarActions
 
 TURN_SIGNAL_BLINK_INTERVAL = 1000
 WAITING_TIME_FONT_SIZE = 20
 TURN_SIGNAL_COLOR = (255, 85, 0)
 
 class Car:
+
+    SPEED = 5
+    WIDTH = 20
+    LENGTH = 40
+
     def __init__(self, window):
 
         self.window = window
         self.window_width = self.window.get_width()
         self.window_height = self.window.get_height()
 
-        self.direction = random.choice(['up', 'down', 'left', 'right'])
+        self.direction = random.choice([CarActions.UP, CarActions.DOWN, CarActions.LEFT, CarActions.RIGHT])
 
-        self.x = None
-        self.y = None
-
-        assert self.direction in ['up', 'down', 'left', 'right']
-
-        if self.direction == 'up':
-            self.x = self.window_width // 2 + 5
-            self.y = self.window_height
-
-        elif self.direction == 'down':
-            self.x = self.window_width//2 - 20 - 4 
-            self.y = 0
-        
-        elif self.direction == 'left':
-            self.x = self.window_width
-            self.y = self.window_height//2 - 20 - 4
-        
-        elif self.direction == 'right':
-            self.x = 0
-            self.y = self.window_height//2 + 5
-
-        self.speed = 2
-        self.width = 20
-        self.length = 40
+        self.x, self.y = self._set_veichle_coordinates(self.direction)
 
         self.stopped = False
         self.color = self._generate_random_color()
         self.turn_right = random.choice([False, True])
         self.waiting_time = 0
 
+    def _set_veichle_coordinates(self, direction: CarActions) -> tuple:
+        """Set the initial coordinates of the vehicle based on the direction it is facing.
+        Args:
+            direction (CarActions): The direction the vehicle is facing.
+        Returns:
+            tuple: The x and y coordinates of the vehicle.
+        """
+
+        if direction == CarActions.UP:
+            return self.window_width // 2 + 5, self.window_height
+        elif direction == CarActions.DOWN:
+            return self.window_width // 2 - 20 - 4, 0
+        elif direction == CarActions.LEFT:
+            return self.window_width, self.window_height // 2 - 20 - 4
+        elif direction == CarActions.RIGHT:
+            return 0, self.window_height // 2 + 5
+
+
     def _generate_random_color(self):
         return (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
 
     def move(self):
+
         if self.stopped:
             self.waiting_time += 1
         else:
-            if self.direction == 'up':
-                self.y -= self.speed
-            elif self.direction == 'down':
-                self.y += self.speed
-            elif self.direction == 'left':
-                self.x -= self.speed
-            elif self.direction == 'right':
-                self.x += self.speed
+            if self.direction == CarActions.UP:
+                self.y -= Car.SPEED
+            elif self.direction == CarActions.DOWN:
+                self.y += Car.SPEED
+            elif self.direction == CarActions.LEFT:
+                self.x -= Car.SPEED
+            elif self.direction == CarActions.RIGHT:
+                self.x += Car.SPEED
 
     def draw(self):
         self._draw_car_body()
@@ -69,10 +71,9 @@ class Car:
         rect = pygame.Rect(
             self.x, 
             self.y,
-            self.width if self.direction in ['up', 'down'] else self.length, 
-            self.length if self.direction in ['up', 'down'] else self.width
-            )
-        
+            Car.WIDTH if self.direction in [CarActions.UP, CarActions.DOWN] else Car.LENGTH, 
+            Car.LENGTH if self.direction in [CarActions.UP, CarActions.DOWN] else Car.WIDTH
+        )
         pygame.draw.rect(self.window, self.color, rect)
 
     def _draw_turn_signal(self):
@@ -81,14 +82,14 @@ class Car:
             pygame.draw.polygon(self.window, TURN_SIGNAL_COLOR, points)
 
     def _calculate_turn_signal_points(self):
-        if self.direction == 'up':
-            return [(self.x + self.width, self.y), (self.x + self.width, self.y + 10), (self.x + self.width + 5, self.y - 5), (self.x + self.width + 5, self.y + 15)]
-        elif self.direction == 'down':
-            return [(self.x, self.y + self.length), (self.x, self.y + self.length - 10), (self.x - 5, self.y + self.length + 5), (self.x - 5, self.y + self.length - 15)]
-        elif self.direction == 'left':
+        if self.direction == CarActions.UP:
+            return [(self.x + Car.WIDTH, self.y), (self.x + Car.WIDTH, self.y + 10), (self.x + Car.WIDTH + 5, self.y - 5), (self.x + Car.WIDTH + 5, self.y + 15)]
+        elif self.direction == CarActions.DOWN:
+            return [(self.x, self.y + Car.LENGTH), (self.x, self.y + Car.LENGTH - 10), (self.x - 5, self.y + Car.LENGTH + 5), (self.x - 5, self.y + Car.LENGTH - 15)]
+        elif self.direction == CarActions.LEFT:
             return [(self.x, self.y), (self.x + 10, self.y), (self.x - 5, self.y - 5), (self.x + 15, self.y - 5)]
         else:  # self.direction == 'right'
-            return [(self.x + self.length, self.y + self.width // 2), (self.x + self.length - 10, self.y + self.width // 2), (self.x + self.length + 5, self.y + self.width // 2 - 5), (self.x + self.length + 5, self.y + self.width // 2 + 5)]
+            return [(self.x + Car.LENGTH, self.y + Car.WIDTH // 2), (self.x + Car.LENGTH - 10, self.y + Car.WIDTH // 2), (self.x + Car.LENGTH + 5, self.y + Car.WIDTH // 2 - 5), (self.x + Car.LENGTH + 5, self.y + Car.WIDTH // 2 + 5)]
 
     def _draw_waiting_time(self):
         font = pygame.font.Font(None, 20)   # font size for waiting time
@@ -102,39 +103,39 @@ class Car:
     def can_move(self, other_cars):
         for other_car in other_cars:
             if other_car.stopped:
-                if self.direction == other_car.direction == 'up' and other_car.y + other_car.length + 4 <= self.y <= other_car.y + other_car.length + 6:
+                if self.direction == other_car.direction == CarActions.UP and other_car.y + Car.LENGTH + 4 <= self.y <= other_car.y + Car.LENGTH + 6:
                     return False
-                elif self.direction == other_car.direction == 'down' and other_car.y - self.length - 6 <= self.y <= other_car.y - self.length - 4:
+                elif self.direction == other_car.direction == CarActions.DOWN and other_car.y - Car.LENGTH - 6 <= self.y <= other_car.y - Car.LENGTH - 4:
                     return False
-                elif self.direction == other_car.direction == 'left' and other_car.x + other_car.length + 4 <= self.x <= other_car.x + other_car.length + 6:
+                elif self.direction == other_car.direction == CarActions.LEFT and other_car.x + Car.LENGTH + 4 <= self.x <= other_car.x + Car.LENGTH + 6:
                     return False
-                elif self.direction == other_car.direction == 'right' and other_car.x - self.length - 6 <= self.x <= other_car.x - self.length - 4:
+                elif self.direction == other_car.direction == CarActions.RIGHT and other_car.x - Car.LENGTH - 6 <= self.x <= other_car.x - Car.LENGTH - 4:
                     return False
         return True
 
     def turn_or_straight(self):
 
         if self.turn_right:
-            if self.direction == 'up' and self.y <= self.window_height // 2:
-                self.direction = 'right'
-                self.x += self.length // 2
+            if self.direction == CarActions.UP and self.y <= self.window_height // 2:
+                self.direction = CarActions.RIGHT
+                self.x += Car.LENGTH // 2
                 self.y = self.window_height // 2 + 5
                 self.turn_right = False
 
-            elif self.direction == 'down' and self.y + self.length >= self.window_height // 2:
-                self.direction = 'left'
-                self.x -= self.length // 2
+            elif self.direction == CarActions.DOWN and self.y + Car.LENGTH >= self.window_height // 2:
+                self.direction = CarActions.LEFT
+                self.x -= Car.LENGTH // 2
                 self.y = self.window_height // 2 - 20 - 4
                 self.turn_right = False
 
-            elif self.direction == 'left' and self.x <= self.window_width // 2:
-                self.direction = 'up'
+            elif self.direction == CarActions.LEFT and self.x <= self.window_width // 2:
+                self.direction = CarActions.UP
                 self.x = self.window_width // 2 + 5
-                self.y -= self.length // 2
+                self.y -= Car.LENGTH // 2
                 self.turn_right = False
 
-            elif self.direction == 'right' and self.x + self.length >= self.window_width // 2:
-                self.direction = 'down'
+            elif self.direction == CarActions.RIGHT and self.x + Car.LENGTH >= self.window_width // 2:
+                self.direction = CarActions.DOWN
                 self.x = self.window_width // 2 - 20 - 4
-                self.y += self.length // 2
+                self.y += Car.LENGTH // 2
                 self.turn_right = False
---
+++ entities/car_actions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from enum import Enum
+
+class CarActions(Enum):
+    UP = "up"
+    DOWN = "down"
+    LEFT = "left"
+    RIGHT = "right"
+
