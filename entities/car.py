c65f7196b3153377cc3f4201152e3273b2bb0307
Davide Capone <davidecapone2304@gmail.com>
2024-07-23 16:51:01 +0200

    Add Ambient class, refactor Car class

--- entities/car.py
+++ entities/car.py
@@ -2,10 +2,6 @@ import random
 import pygame
 from entities.car_actions import CarActions
 
-TURN_SIGNAL_BLINK_INTERVAL = 1000
-WAITING_TIME_FONT_SIZE = 20
-TURN_SIGNAL_COLOR = (255, 85, 0)
-
 class Car:
 
     SPEED = 5
@@ -19,13 +15,13 @@ class Car:
         self.window_height = self.window.get_height()
 
         self.direction = random.choice([CarActions.UP, CarActions.DOWN, CarActions.LEFT, CarActions.RIGHT])
-
         self.x, self.y = self._set_veichle_coordinates(self.direction)
 
-        self.stopped = False
-        self.color = self._generate_random_color()
+        self.isStopped = False
         self.turn_right = random.choice([False, True])
         self.waiting_time = 0
+
+        self.color = self._generate_random_color()
 
     def _set_veichle_coordinates(self, direction: CarActions) -> tuple:
         """Set the initial coordinates of the vehicle based on the direction it is facing.
@@ -46,11 +42,16 @@ class Car:
 
 
     def _generate_random_color(self):
+        """Generate a random color for the vehicle.
+
+        Returns:
+            tuple: The RGB values of the color.
+        """
         return (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
 
     def move(self):
 
-        if self.stopped:
+        if self.isStopped:
             self.waiting_time += 1
         else:
             if self.direction == CarActions.UP:
@@ -63,25 +64,32 @@ class Car:
                 self.x += Car.SPEED
 
     def draw(self):
-        self._draw_car_body()
+        pygame.draw.rect(self.window, self.color, self._generate_car_rect())
+
         self._draw_turn_signal()
         self._draw_waiting_time()
 
-    def _draw_car_body(self):
+    def _generate_car_rect(self):
+
         rect = pygame.Rect(
             self.x, 
             self.y,
             Car.WIDTH if self.direction in [CarActions.UP, CarActions.DOWN] else Car.LENGTH, 
             Car.LENGTH if self.direction in [CarActions.UP, CarActions.DOWN] else Car.WIDTH
         )
-        pygame.draw.rect(self.window, self.color, rect)
+        return rect
 
     def _draw_turn_signal(self):
+
+        TURN_SIGNAL_BLINK_INTERVAL = 1000
+        TURN_SIGNAL_COLOR = (255, 85, 0)
+
         if self.turn_right and pygame.time.get_ticks() // TURN_SIGNAL_BLINK_INTERVAL % 2 == 0:
             points = self._calculate_turn_signal_points()
-            pygame.draw.polygon(self.window, TURN_SIGNAL_COLOR, points)
+            pygame.draw.polygon(self.window, TURN_SIGNAL_COLOR, points) 
 
     def _calculate_turn_signal_points(self):
+
         if self.direction == CarActions.UP:
             return [(self.x + Car.WIDTH, self.y), (self.x + Car.WIDTH, self.y + 10), (self.x + Car.WIDTH + 5, self.y - 5), (self.x + Car.WIDTH + 5, self.y + 15)]
         elif self.direction == CarActions.DOWN:
@@ -98,11 +106,11 @@ class Car:
         self.window.blit(text, (self.x + 5, self.y + 5))
 
     def stop(self):
-        self.stopped = True
+        self.isStopped = True
 
     def can_move(self, other_cars):
         for other_car in other_cars:
-            if other_car.stopped:
+            if other_car.isStopped:
                 if self.direction == other_car.direction == CarActions.UP and other_car.y + Car.LENGTH + 4 <= self.y <= other_car.y + Car.LENGTH + 6:
                     return False
                 elif self.direction == other_car.direction == CarActions.DOWN and other_car.y - Car.LENGTH - 6 <= self.y <= other_car.y - Car.LENGTH - 4:
