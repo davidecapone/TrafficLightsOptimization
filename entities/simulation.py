774a8b529becc7da9dd6023a9d792574b65520e6
Davide Capone <davidecapone2304@gmail.com>
2024-07-24 14:53:16 +0200

    Improve code readability and flow management at intersection points

--- entities/simulation.py
+++ entities/simulation.py
@@ -16,6 +16,7 @@ class Simulation():
         assert window_size[0] > 0 and window_size[1] > 0, "Window size must be greater than 0"
 
         pygame.init()
+        pygame.display.set_caption(name)
 
         self.window = pygame.display.set_mode(window_size)
         self.window_width = self.window.get_width()
@@ -23,51 +24,31 @@ class Simulation():
 
         self.ambient_images = self._load_images(ambient_images_path)
         self.ambient_images = self._resize_images(self.ambient_images)
-        self._blit_images()
 
         if audio_effect_path:
             self._load_audio(audio_effect_path, volume = 0.2)
 
-        self._draw_lines()
-
         self.clock = pygame.time.Clock()
         self.cars = []
 
-        pygame.display.set_caption(name)
-
 
     def _load_images(self, ambient_images_path:list) -> None:
-            
-            ambient_images = []
-    
-            for image_path in ambient_images_path:
-                ambient_images.append(pygame.image.load(image_path))
-    
-            return ambient_images
-
-            # using list com
+        return [pygame.image.load(image_path) for image_path in ambient_images_path]
     
     def _resize_images(self, ambient_images:list) -> None:
-         
-        for i in range(len(ambient_images)):
-            ambient_images[i] = pygame.transform.scale(
-                 ambient_images[i], (self.window_width//2 - 30, self.window_height//2 - 30))
-    
-        return ambient_images
+        return [pygame.transform.scale(image, (self.window_width//2 - 30, self.window_height//2 - 30)) for image in ambient_images]
     
     def _blit_images(self) -> None:
-    
         self.window.blit(self.ambient_images[0], (0, 0))
         self.window.blit(self.ambient_images[1], (self.window_width//2 + 30, 0))
         self.window.blit(self.ambient_images[2], (self.window_width//2 + 30, self.window_height//2 + 30))
         self.window.blit(self.ambient_images[3], (0, self.window_height//2 + 30))
     
     def _load_audio(self, audio_effect_path:str, volume:float) -> None:
-
-            pygame.mixer.init()
-            pygame.mixer.music.load(audio_effect_path)
-            pygame.mixer.music.set_volume(volume)
-            pygame.mixer.music.play(-1)
+        pygame.mixer.init()
+        pygame.mixer.music.load(audio_effect_path)
+        pygame.mixer.music.set_volume(volume)
+        pygame.mixer.music.play(-1)
 
     def _draw_lines(self):
         # Draw intersection
@@ -89,8 +70,7 @@ class Simulation():
         # Cover intersection
         pygame.draw.rect(self.window, GRAY, (self.window_width // 2 - 29, self.window_height // 2 - 29, 60, 60))
 
-    def draw_stoplight(self, stoplight:Stoplight):
-
+    def _draw_stoplight(self, stoplight:Stoplight):
         pygame.draw.line(self.window, stoplight.color_NS, (self.window_width//2 - 27, self.window_height//2 - 32), (self.window_width//2 - 2, self.window_height//2 - 32), 5)
         pygame.draw.line(self.window, stoplight.color_NS, (self.window_width//2 + 3, self.window_height//2 + 33), (self.window_width//2 + 27, self.window_height//2 + 33), 5)
         pygame.draw.line(self.window, stoplight.color_EW, (self.window_width//2 - 32, self.window_height//2 + 3), (self.window_width//2 - 32, self.window_height//2 + 27), 5)
@@ -106,7 +86,7 @@ class Simulation():
             # Draw the ambient
             self._blit_images()
             self._draw_lines()
-            self.draw_stoplight(stoplight)
+            self._draw_stoplight(stoplight)
 
             self.clock.tick(30)
             stoplight.update_stoplight()
@@ -129,36 +109,53 @@ class Simulation():
 
             for car in self.cars:
 
-                car_direction = car.get_direction()
-
                 if car.is_stopped():
                     car.increase_waiting_time() # Then we increase the waiting time
+                    car_direction = car.get_direction() # Get the direction of the car
 
                     if ((car_direction in [CarActions.UP, CarActions.DOWN] and stoplight.color_NS == TrafficLightColor.GREEN.value) or 
                         (car_direction in [CarActions.LEFT, CarActions.RIGHT] and stoplight.color_EW == TrafficLightColor.GREEN.value)):
                         car.set_stopped(False)
                         car.move()
 
-                else:   # Car is moving
-                    car_x, car_y = car.get_position()
-
-                    if ((car_direction == CarActions.UP and car_y == self.window_height//2 + 50 and (stoplight.color_NS in [TrafficLightColor.RED.value, TrafficLightColor.YELLOW.value])) or
-                        (car_direction == CarActions.DOWN and car_y + Car.LENGTH == self.window_height//2 - 50 and (stoplight.color_NS in [TrafficLightColor.RED.value, TrafficLightColor.YELLOW.value])) or
-                        (car_direction == CarActions.LEFT and car_x == self.window_width//2 + 50 and (stoplight.color_EW in [TrafficLightColor.RED.value, TrafficLightColor.YELLOW.value])) or
-                        (car_direction == CarActions.RIGHT and car_x + Car.LENGTH == self.window_width//2 - 50 and (stoplight.color_EW in [TrafficLightColor.RED.value, TrafficLightColor.YELLOW.value]))) or not car.can_move(self.cars):
-                        car.set_stopped(True)
-                    else:
-                        if ((car_direction == CarActions.UP and car_y <= self.window_height//2 + Car.SPEED and car_y >= self.window_height//2 - Car.SPEED) or
-                            (car_direction == CarActions.DOWN and car_y + Car.LENGTH >= self.window_height//2 - Car.SPEED and car_y + Car.LENGTH <= self.window_height//2 + Car.SPEED) or
-                            (car_direction == CarActions.LEFT and car_x <= self.window_width//2 + Car.SPEED and car_x >= self.window_width//2 - Car.SPEED) or
-                            (car_direction == CarActions.RIGHT and car_x + Car.LENGTH >= self.window_width//2 - Car.SPEED and car_x + Car.LENGTH <= self.window_width//2 + Car.SPEED)):
-                            car.turn_or_straight()
-                        car.move()
+                elif self.is_at_intersection(car) and self.should_stop(car, stoplight) or not car.can_move(self.cars):
+                    car.set_stopped(True)
+                else:   
+                    car.turn_or_straight()
+                    car.move()
 
                 # Remove cars that have left the screen
                 if car.is_out_of_window():
                     self.cars.remove(car)
 
                 car.draw()
-                
             pygame.display.update()
+
+    def should_stop(self, car, stoplight):
+
+        x, y = car.get_position()
+        car_direction = car.get_direction()
+
+        mid_x, mid_y = self.window_width // 2, self.window_height // 2
+
+        return (
+            (car_direction == CarActions.UP and y == mid_y + 50 and stoplight.color_NS in [TrafficLightColor.RED.value, TrafficLightColor.YELLOW.value]) or
+            (car_direction == CarActions.DOWN and y + Car.LENGTH == mid_y - 50 and stoplight.color_NS in [TrafficLightColor.RED.value, TrafficLightColor.YELLOW.value]) or
+            (car_direction == CarActions.LEFT and x == mid_x + 50 and stoplight.color_EW in [TrafficLightColor.RED.value, TrafficLightColor.YELLOW.value]) or
+            (car_direction == CarActions.RIGHT and x + Car.LENGTH == mid_x - 50 and stoplight.color_EW in [TrafficLightColor.RED.value, TrafficLightColor.YELLOW.value])
+        )
+    
+    def is_at_intersection(self, car):
+
+        car_direction = car.get_direction()
+        x, y = car.get_position()
+
+        mid_x, mid_y = self.window_width // 2, self.window_height // 2
+        offset = 50
+
+        return (
+            (car_direction == CarActions.UP and mid_y - offset <= y <= mid_y + offset) or
+            (car_direction == CarActions.DOWN and mid_y - offset <= y + Car.LENGTH <= mid_y + offset) or
+            (car_direction == CarActions.LEFT and mid_x - offset <= x <= mid_x + offset) or
+            (car_direction == CarActions.RIGHT and mid_x - offset <= x + Car.LENGTH <= mid_x + offset)
+        )
